8f1abbf4c682cc2c5d48903750530c332e013bff
Markus Montonen <montonen.markus@gmail.com>
2026-05-15 06:52:22 +0300

    Layout changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,38 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Asset } from "expo-asset";
 import { Image } from 'expo-image';
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
 import Head from 'expo-router/head';
 import { cssInterop } from "nativewind";
 import { useTranslation } from 'react-i18next';
-import { View } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
 import '../i18n/config';
 import './global.css';
 
 const logoImageSource = { uri: Asset.fromModule(require('../assets/images/Finnut-Logo-white.webp')).uri };
 
-function LogoTitle() {
+function LogoTitle({ onPress }: { onPress: () => void }) {
   return (
-    <Image
-      allowDownscaling={false}
-      contentFit="cover"
-      style={{ width: 110, height: 50, margin: 16 }}
-      source={logoImageSource} />
+    <TouchableOpacity onPress={onPress}>
+      <Image
+        allowDownscaling={false}
+        contentFit="cover"
+        style={{ width: 110, height: 50, margin: 16 }}
+        source={logoImageSource} />
+    </TouchableOpacity>
+  );
+}
+
+function HeaderActions() {
+  return (
+    <View className="flex-row gap-1">
+      <TouchableOpacity>
+        <FontAwesome.Button name="search" backgroundColor="bg-primary-500"></FontAwesome.Button>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <FontAwesome.Button name="shopping-cart" backgroundColor="bg-primary-500"></FontAwesome.Button>
+      </TouchableOpacity>
+    </View>
   );
 }
 
@@ -26,6 +42,11 @@ const StyledView = cssInterop(View, {
 
 function RootStack() {
   const { t } = useTranslation();
+  const router = useRouter();
+
+  const handleLogoPress = () => {
+    router.navigate('/');
+  }
 
   return (
     <Stack screenOptions={{
@@ -33,14 +54,13 @@ function RootStack() {
       headerBackground: () => <StyledView className="h-20 bg-primary-500 p-4" />,
       headerTintColor: '#fff',
       headerTransparent: true,
-      headerLeft: () => <LogoTitle />,
+      headerLeft: () => <LogoTitle onPress={handleLogoPress} />,
+      headerRight: () => <HeaderActions />,
+      headerTitle: '',
       contentStyle: { paddingTop: 80 },
     }}>
       <Stack.Screen
         name="index"
-        options={{
-          title: t('common.title'),
-        }}
       />
     </Stack>
   );
